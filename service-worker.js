c6f9e340964b76a2745e50e20e8df74246471a59
yanmagithub <yanma121@gmail.com>
2018-12-10 16:05:58 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "./precache-manifest.906f9a4d47dae07b1c2c9e4dd9524be4.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "ggg_env"});
+
+workbox.skipWaiting();
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "index.html",
+    "revision": "68e2f169ce315cfba254c4d15a979a06"
+  },
+  {
+    "url": "service-worker.js",
+    "revision": "e642bf924e34763a0e80a886cb2c974f"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
